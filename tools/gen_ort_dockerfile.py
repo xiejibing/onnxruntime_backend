a3c7db52a9648cca3c128cbc94948fc436f21116
xiejibing <33129072+xiejibing@users.noreply.github.com>
2025-05-06 16:16:27 +0800

    Update onnxruntime url in gen_ort_dockerfile.py

--- tools/gen_ort_dockerfile.py
+++ tools/gen_ort_dockerfile.py
@@ -54,7 +54,7 @@ def dockerfile_common():
     df = """
 ARG BASE_IMAGE={}
 ARG ONNXRUNTIME_VERSION={}
-ARG ONNXRUNTIME_REPO=https://github.com/microsoft/onnxruntime
+ARG ONNXRUNTIME_REPO=https://github.com/xiejibing/onnxruntime
 ARG ONNXRUNTIME_BUILD_CONFIG={}
 """.format(
         FLAGS.triton_container, FLAGS.ort_version, FLAGS.ort_build_config
